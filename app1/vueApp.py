e3d150827926539fc78b73995988e79be4b98d21
Anthony Seys <antho.moi.62730@gmail.com>
2024-05-07 10:47:21 +0200

    Update vueApp1

---
+++ app1/vueApp.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication
+
+
+class Image(QPixmap):
+ 
+    def __init__(self):
+        '''Constructeur de la classe'''
+
+        # appel au constructeur de la classe mère
+        super().__init__() 
+        
+        self.image = QPixmap('../models/plan2.png')
+        self.setPixmap(self.image)
+
+        self.show()
+
+
+# --- main --------------------------------------------------------------------
+if __name__ == "__main__":
+
+    # création d'une QApplication
+    app = QApplication(sys.argv)
+
+    # # création de la fenêtre de l'application
+    fenetre = Image()
+
+    # lancement de l'application
+    sys.exit(app.exec())
