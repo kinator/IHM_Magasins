90290dc9535fa28292e915dbbdf962f8ba3360d3
Fydyr <fournierenzo62@gmail.com>
2024-05-15 14:59:36 +0200

    mis à jour code

--- app1/produit.py
+++ app1/produit.py
@@ -6,6 +6,10 @@ from cadrillage import Case
 class Produit:
 
     #constructeur
+    def __init__(self):
+        self.nom = None
+        self.emplacement = Case((0, 0), True)
+
     def __init__(self, nom_prod : str, x : int, y : int):
         self.nom = nom_prod
         self.emplacement = Case((x, y), True)
